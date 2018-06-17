7e0038101968770f356379981cff8d65628d2be2
Andrew Moe <andrewmoe@Andrews-MacBook-Pro.local>
2018-06-17 13:13:08 -0500

    updated readme

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 The Android side of the Shopcyclops project. Shopping through another person using streaming video.
 
-Think twitch for shopping!
+Flow of the app (when secrets are included):
+1. Select your delivery location on a map.
+2. View available streamers in your area.
+3. Join their streams and add things to their shopping carts.
+4. Checkout before they checkout (guaranteeing payment for them)
+5. The streamer is giving the most optimal map for their deliveries.
+6. Your order is delivered to you ASAP!
+
+The app currently has had all the secrets removed for safety.
+
+This app ties into a server which manages streams and orders. Currently not deployed.
 
 ## any questions? :jack_o_lantern:
 Contact Lemonlemons (Andrew Moe)
--- app/src/main/java/com/shopcyclops/CONSTANTS.java
+++ app/src/main/java/com/shopcyclops/CONSTANTS.java
@@ -6,8 +6,8 @@ package com.shopcyclops;
 public class CONSTANTS {
     public static final String BASE_URL = "http://4daf86f3.ngrok.com";
 
-    public static final String KICKFLIP_CLIENT_KEY = "B_wi=z4n8GnqsfE4CP!pMpFa19hvCVVD_0cpOAxD";
-    public static final String KICKFLIP_CLIENT_SECRET = "evUnof;OX_7=1Kwv7s;2LPu=m:RPiu6rM!V5n3WHi2sj2kR-82.lzP=Wn1SmN4P1e!ec:6.baJKJwgqbTEiwUNgmn4M:Uj6!q9ixLN7BbQrdGpD483FqO4Oy6@o.@sJf";
+    public static final String KICKFLIP_CLIENT_KEY = "";
+    public static final String KICKFLIP_CLIENT_SECRET = "";
 
     public static final String TOKEN_KEY = "TOKEN";
     public static final String EMAIL_KEY = "USER_EMAIL";
@@ -28,14 +28,14 @@ public class CONSTANTS {
     public static final String CURRENT_CARDCODE = "current credit code";
     public static final String IS_COMPLETE_STREAM = "is the stream complete?";
 
-    public static final String STRIPE_CLIENT_ID = "ca_5qamk5Dt98nk4LcYpzrgDgYk6ARmn4rw";
-    public static final String STRIPE_SECRET_KEY = "sk_test_FQN03AnepMDNcZaFFduIsVJS";
-    public static final String STRIPE_PUBLIC_KEY = "pk_test_7CMIjTOkDl8S6fkOImYJ8Qz5";
+    public static final String STRIPE_CLIENT_ID = "";
+    public static final String STRIPE_SECRET_KEY = "";
+    public static final String STRIPE_PUBLIC_KEY = "";
     public static final String STRIPE_CALLBACK_URL = BASE_URL+"/users/auth/stripe_connect/callback";
     public static final String STRIPE_TOKEN_KEY = "stripe_token_key";
 
-    public static final String PUSHER_KEY = "efb3250cf7fd30c78d49";
-    public static final String PUSHER_SECRET = "2d9da406190b5c9dd3ed";
+    public static final String PUSHER_KEY = "";
+    public static final String PUSHER_SECRET = "";
     public static final String PUSHER_APP_ID = "133847";
     public static final String PUSHER_AUTH_ENDPOINT = BASE_URL+"/auth";
 
@@ -55,10 +55,10 @@ public class CONSTANTS {
     public static final String CURRENT_STREAM_HOME_POINT_LAT = "latitude of the final waypoint";
     public static final String CURRENT_STREAM_HOME_POINT_LNG = "longitude of the final waypoint";
 
-    public static final String AWS_ACCESS_KEY = "AKIAJIYKCLZ3HC5MVDNQ";
-    public static final String AWS_SECRET_KEY = "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";
+    public static final String AWS_ACCESS_KEY = "";
+    public static final String AWS_SECRET_KEY = "";
     public static final String AWS_BUCKET_NAME = "shopcyclops";
     public static final String AWS_IMAGE_URL = "https://s3-us-west-2.amazonaws.com/shopcyclops/";
 
-    public static final String GOOGLE_MAPS_KEY = "AIzaSyA8P6lCZ8eRjE1Hilp94HhHyHTiG6Kk-k4";
+    public static final String GOOGLE_MAPS_KEY = "";
 }
